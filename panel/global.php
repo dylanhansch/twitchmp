4a331c202a8f92f7aacacdfa007a6f292b02fabb
Dylan Hansch <dylan@dylanhansch.net>
2014-12-27 20:02:27 -0600

    Cleanup
    Removed unnecessary lines of code and cleaned weird code..

--- panel/global.php
+++ panel/global.php
@@ -3,7 +3,7 @@ session_name("twitchMP");
 session_start();
 require_once('protected/config.php');
 
-//checking is the sessions are set
+// Checking if the sessions are set
 if(isset($_SESSION['username']) && isset($_SESSION['pass']) && isset($_SESSION['id'])){
 	$session_username = $_SESSION['username'];
 	$session_pass = $_SESSION['pass'];
@@ -67,93 +67,8 @@ if(isset($_SESSION['id'])){
 	$stmt->close();
 }
 
-// List links you've got permission to in a table on index.php
-function links(){
-	global $mysqli, $logged, $role, $session_id;
-	if($role == "admin"){
-		$stmt = $mysqli->prepare("SELECT links.id, links.short, links.url, links.privacy, users.username FROM `links` INNER JOIN `privileges` ON privileges.link_id = links.id INNER JOIN `users` ON users.id = privileges.user_id");
-		echo($mysqli->error);
-		$stmt->execute();
-		$stmt->bind_result($out_id,$out_short,$out_url,$out_privacy,$out_owner);
-		$links = array();
-
-		while($stmt->fetch()){
-			$links[] = array('id' => $out_id, 'short' => $out_short, 'url' => $out_url, 'privacy' => $out_privacy, 'owner' => $out_owner);
-		}
-		$stmt->close();
-
-		return $links;
-	}elseif($logged == 1){
-		$stmt = $mysqli->prepare("SELECT links.id, links.short, links.url, links.privacy, users.username FROM `links` INNER JOIN `privileges` ON privileges.link_id = links.id INNER JOIN `users` ON users.id = privileges.user_id WHERE privacy = 'public' OR privileges.user_id = ?");
-		echo($mysqli->error);
-		$stmt->bind_param('i', $session_id);
-		$stmt->execute();
-		$stmt->bind_result($out_id,$out_short,$out_url,$out_privacy,$out_owner);
-		$links = array();
-		
-		while($stmt->fetch()){
-			$links[] = array('id' => $out_id, 'short' => $out_short, 'url' => $out_url, 'privacy' => $out_privacy, 'owner' => $out_owner);
-		}
-		$stmt->close();
-		
-		return $links;
-	}else{
-		$stmt = $mysqli->prepare("SELECT id,short,url FROM `links` WHERE `privacy` = 'public'");
-		echo($mysqli->error);
-		$stmt->execute();
-		$stmt->bind_result($out_id,$out_short,$out_url);
-		$links = array();
-
-		while($stmt->fetch()){
-			$links[] = array('id' => $out_id, 'short' => $out_short, 'url' => $out_url);
-		}
-		$stmt->close();
-
-		return $links;
-	}
-}
-
-// Delete specified link from database
-function del_link($link_id){
-	global $mysqli, $session_id, $role;
-	
-	if(isset($session_id) && $role == "admin"){
-		$stmt = $mysqli->prepare("DELETE FROM links WHERE id = ?");
-		echo($mysqli->error);
-		$stmt->bind_param("i", $link_id);
-		$stmt->execute();
-		$stmt->close();
-
-		$stmt = $mysqli->prepare("DELETE FROM privileges WHERE link_id = ?");
-		echo($mysqli->error);
-		$stmt->bind_param("i", $link_id);
-		$stmt->execute();
-		$stmt->close();
-	}elseif(isset($session_id)){
-		$stmt = $mysqli->prepare("SELECT role FROM privileges WHERE user_id = ? AND link_id = ?");
-		echo($mysqli->error);
-		$stmt->bind_param('ii', $session_id, $link_id);
-		$stmt->execute();
-		$stmt->bind_result($perm);
-		if(!($stmt->fetch())){
-			$perm = "view";
-		}
-		$stmt->close();
-		
-		if($perm != "view"){
-			$stmt = $mysqli->prepare("DELETE FROM links WHERE id = ?");
-			echo($mysqli->error);
-			$stmt->bind_param("i", $link_id);
-			$stmt->execute();
-			$stmt->close();
-
-			$stmt = $mysqli->prepare("DELETE FROM privileges WHERE link_id = ?");
-			echo($mysqli->error);
-			$stmt->bind_param("i", $link_id);
-			$stmt->execute();
-			$stmt->close();
-		}else{
-			die("No permission.");
-		}
-	}
+// Clean the input of possibly malicious or interfering code
+function clean($input){
+	$output = preg_replace("#[^0-9]#","",$input);
+	return $output;
 }
--- panel/index.php
+++ panel/index.php
@@ -22,8 +22,6 @@ require_once('global.php');
 			<div class="row">
 				<div class="col-lg-12">
 					<h1><?php echo($app); ?> <small>Choose which user to stream to a single channel</small></h1>
-					
-					<iframe src="http://www.twitch.tv/dylanhansch/embed" frameborder="0" scrolling="no" height="378" width="620" class="center"></iframe><a href="http://www.twitch.tv/dylanhansch?tt_medium=live_embed&tt_content=text_link" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px;text-decoration:underline;"></a>
 				</div>
 			</div>
 		</div>
